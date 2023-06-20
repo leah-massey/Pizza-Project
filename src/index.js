cff510c79595dba6ff78b7cc82d326b218a3f9dc
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-06-20 17:38:45 +0100

    Destructuring props for closeHour.

--- src/index.js
+++ src/index.js
@@ -141,12 +141,10 @@ function Footer() {
   );
 }
 
-function Order(props) {
+function Order({ closeHour }) {
   return (
     <div className="order">
-      <p>
-        We're open until {props.closeHour}:00. Come visit us or order online.{" "}
-      </p>
+      <p>We're open until {closeHour}:00. Come visit us or order online. </p>
       <button className="btn">Order</button>
     </div>
   );
